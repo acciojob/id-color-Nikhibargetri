2a6b20aa4a4c1edae168a5670163d6b630e830d3
Nikhibargetri <116011446+Nikhibargetri@users.noreply.github.com>
2025-08-30 23:10:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+// For this task, JS is not required for styling.
+// You can use JS if you want to dynamically change styles or content.
+
+console.log("Paragraphs styled with CSS using ID selectors.");
